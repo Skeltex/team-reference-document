e08d1454a6a468a301ec3856d3d6bb33227ba5bd
Dmitriy Afanasyev <dmtr.afanasyev@gmail.com>
2026-07-21 20:25:13 +0400

    feat: rewrite sqrt insert

--- Scripts/Data structures/sqrt_insert.cpp
+++ Scripts/Data structures/sqrt_insert.cpp
@@ -1,49 +1,37 @@
-int mx[N];
-vector<int> m[N];
+struct SQRT {
+    int n;
+    const int SZ = 2'500;
+    vector<vector<int>> s;
 
-void per() {
-    vector<int> els;
-    for (int i = 0; i < N; i++) {
-        for (auto j : m[i]) els.push_back(j);
-        m[i] = {};
-        mx[i] = 0;
+    vector<int> get_a() {
+        vector<int> a;
+        a.reserve(n);
+        for (const auto& block : s)
+            a.insert(a.end(), all(block));
+        return a;
     }
-    int cur = 0;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            mx[i] = max(mx[i], els[cur]);
-            m[i].push_back(els[cur++]);
-            if (cur == els.size()) return;
-        }
-    }
-}
 
-void solve() {
-    int n; cin >> n;
-    map<int, int> mp;
-    vector<int> a(n), c(n);
-    for (int i = 0; i < n; i++) cin >> a[i] >> c[i], mp[a[i]] = i + 1;
-    for (int i = 0; i < n; i++) {
-        int j = N - 1;
-        for (; j >= 0; j--) {
-            if (c[i] - (int)m[j].size() < 0 or mx[j] > a[i]) break;
-            c[i] -= (int)m[j].size();
+    void insert(int pos, int val) {
+        int i = 0;
+        while (pos > sz(s[i])) {
+            pos -= sz(s[i]);
+            ++i;
         }
-        if (j == -1) {
-            mx[0] = max(mx[0], a[i]);
-            m[0].insert(m[0].begin(), a[i]);
-            continue;
+
+        s[i].insert(s[i].begin() + pos, val);
+        ++n;
+
+        if (sz(s[i]) > 2 * SZ) {
+            vector<int> new_block(s[i].begin() + SZ, s[i].end());
+            new_block.reserve(2 * SZ + 10);
+            s[i].resize(SZ);
+            s.insert(s.begin() + i + 1, new_block);
         }
-        int k = m[j].size() - 1;
-        for (; k >= 0; k--) {
-            if (c[i] == 0 or m[j][k] > a[i]) break;
-            c[i]--;
-        }
-        m[j].insert(m[j].begin() + k + 1, a[i]);
-        mx[j] = max(mx[j], a[i]);
-        if (i % N == 0) per();
     }
-    for (int i = 0; i < N; i++) {
-        for (auto j : m[i]) cout << mp[j] << ' ';
+
+    SQRT() : n(0) {
+        s.reserve(600);
+        s.push_back(vector<int>());
+        s[0].reserve(2 * SZ + 10);
     }
-}
+};
