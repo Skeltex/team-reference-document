a0641ec719df2cc421d96dfc4818e2c0107a82bb
Gosha <MironovGO007@gmail.com>
2026-09-25 21:14:18 +0400

    feat: rewrote MO using the Hilbert curve

--- Scripts/Basic algorithms/mo.cpp
+++ Scripts/Basic algorithms/mo.cpp
@@ -1,56 +1,37 @@
-const int M = 300;
-const int N = 100'013;
-
-int block[N];
-int lg[N];
-int rg[N];
-
-bool cmp(int i, int j) {
-    if (block[i] != block[j]) return block[i] < block[j];
-    if (block[i] & 1) return rg[i] > rg[j];
-    return rg[i] < rg[j];
+int lg[N], rg[N];
+int64_t ord[N];
+inline int64_t hilbertOrder(int x, int y) {
+    int64_t d = 0;
+    for (int s = 1 << 20; s > 0; s >>= 1) {
+        int rx = (x & s) > 0, ry = (y & s) > 0;
+        d = (d << 2) | ((rx * 3) ^ ry);
+        if (!ry) {
+            if (rx) { x = ~x; y = ~y; }
+            swap(x, y);
+        }
+    }
+    return d;
 }
-
+bool cmp(int i, int j) { return ord[i] < ord[j]; }
 void solve() {
     unordered_map<int, int> cnt;
     int n, q;
     cin >> n >> q;
-    vector<int> a(n);
-    for (auto& i : a)
-        cin >> i;
-    vector<int> zp(q);
+    vector<int> a(n), ans(q), zp(q);
+    for (auto& i : a) cin >> i;
     for (int i = 0; i < q; ++i) {
         cin >> lg[i] >> rg[i];
-        --lg[i];
-        block[i] = lg[i] / M;
+        ord[i] = hilbertOrder(--lg[i], rg[i]);
         zp[i] = i;
     }
     sort(all(zp), cmp);
     int l = lg[zp[0]], r = l, cur = 0;
-    vector<int> ans(q);
     for (auto i : zp) {
-        while (l > lg[i]) {
-            --l;
-            if (++cnt[a[l]] == a[l]) ++cur;
-            else if (cnt[a[l]] == a[l] + 1) --cur;
-        }
-        while (r < rg[i]) {
-            if (++cnt[a[r]] == a[r]) ++cur;
-            else if (cnt[a[r]] == a[r] + 1) --cur;
-            ++r;
-        }
-        while (l < lg[i]) {
-            if (cnt[a[l]]-- == a[l]) --cur;
-            else if (cnt[a[l]] == a[l]) ++cur;
-            ++l;
-        }
-        while (r > rg[i]) {
-            --r;
-            if (cnt[a[r]]-- == a[r]) --cur;
-            else if (cnt[a[r]] == a[r]) ++cur;
-        }
+        while (l > lg[i]) { --l; if (++cnt[a[l]] == a[l]) ++cur; else if (cnt[a[l]] == a[l] + 1) --cur; }
+        while (r < rg[i]) { if (++cnt[a[r]] == a[r]) ++cur; else if (cnt[a[r]] == a[r] + 1) --cur; ++r; }
+        while (l < lg[i]) { if (cnt[a[l]]-- == a[l]) --cur; else if (cnt[a[l]] == a[l]) ++cur; ++l; }
+        while (r > rg[i]) { --r; if (cnt[a[r]]-- == a[r]) --cur; else if (cnt[a[r]] == a[r]) ++cur; }
         ans[i] = cur;
     }
-    for (auto i : ans)
-        cout << i << '\n';
+    for (auto i : ans) cout << i << '\n';
 }
